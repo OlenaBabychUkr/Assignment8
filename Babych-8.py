48ff7f9e7586605ebc1799f0bebb7c3cc7f890a2
OlenaBabychUkr <148278289+OlenaBabychUkr@users.noreply.github.com>
2023-10-18 13:07:47 +0300

    Add files via upload

--- Babych-8.py
+++ Babych-8.py
@@ -13,10 +13,10 @@ print([i+1 for i in range(len(cats_in_hat)) if cats_in_hat[i] == True])
 
 import math
 cats_with_hats = []
-    for cat in range(1, 101):
-        #perfect square has an odd number of factors 
-        if math.sqrt(cat) % 1 == 0: 
-            cats_with_hats.append(cat)
+for cat in range(1, 101):
+    #perfect square has an odd number of factors 
+    if math.sqrt(cat) % 1 == 0: 
+        cats_with_hats.append(cat)
 
 print(cats_with_hats)
     
